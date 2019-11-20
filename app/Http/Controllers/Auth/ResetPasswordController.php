2d780d0ad35a8e27898299dcdb8285ca04078e53
WangJunjie <wjj19890503@gmail.com>
2019-11-20 15:04:20 +0000

    重置密码

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -25,5 +25,9 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
 }
